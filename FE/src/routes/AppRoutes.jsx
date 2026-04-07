2c0b5e1ef93f4b0522565b165926fd6604acb3f0
nguyen dang viet <vietnguyen.1022005@gmail.com>
2026-04-07 08:57:37 +0700

    thêm font roboto, cập nhật index.css, thêm config route cho dự án

---
+++ FE/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../pages/auth/login/Login';
+import Dashboard from '../pages/Dashboard';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
