d64fc45cbbbe47d121659c9ea26ef0bc0511302d
huyhoangy <huyhoangwf@gmail.com>
2026-04-08 22:18:04 +0700

    fix loi routes doan khoa

--- FE/src/routes/DoanKhoaRoutes.jsx
+++ FE/src/routes/DoanKhoaRoutes.jsx
@@ -1,4 +1,6 @@
 import { Route } from 'react-router-dom';
+import { ROLES } from '@/constants/roles';
+import ProtectedRoute from './ProtectedRoute';
 import DashboardLayout from '@/components/layouts/dashboardLayout/DashboardLayout';
 
 // Pages – Đoàn Khoa
@@ -8,21 +10,23 @@ import HoatDongKhoaDuyet from '@/pages/doankhoa/hoat-dong-khoa/HoatDongKhoaDuyet
 import HoatDongKhoaXacNhan from '@/pages/doankhoa/hoat-dong-khoa/HoatDongKhoaXacNhan';
 import YeuCauChiDoan from '@/pages/doankhoa/yeu-cau-chi-doan/YeuCauChiDoan';
 import GuiYeuCau from '@/pages/doankhoa/gui-yeu-cau/GuiYeuCau';
+
 import BaoCao from '@/pages/doankhoa/bao-cao/BaoCao';
 
 /**
  * DoanKhoaRoutes – prefix /doan-khoa (theo convention của main)
- * TODO: Bật lại ProtectedRoute khi tích hợp BE
  */
 const DoanKhoaRoutes = (
-  <Route path="/doan-khoa" element={<DashboardLayout />}>
-    <Route path="dashboard" element={<DashboardKhoa />} />
-    <Route path="hoat-dong-khoa/quan-ly" element={<HoatDongKhoaQuanLy />} />
-    <Route path="hoat-dong-khoa/duyet" element={<HoatDongKhoaDuyet />} />
-    <Route path="hoat-dong-khoa/xac-nhan" element={<HoatDongKhoaXacNhan />} />
-    <Route path="yeu-cau-chi-doan" element={<YeuCauChiDoan />} />
-    <Route path="gui-yeu-cau" element={<GuiYeuCau />} />
-    <Route path="bao-cao" element={<BaoCao />} />
+  <Route element={<ProtectedRoute allowedRoles={[ROLES.DOANKHOA]} />}>
+    <Route path="/doan-khoa" element={<DashboardLayout />}>
+      <Route path="dashboard" element={<DashboardKhoa />} />
+      <Route path="hoat-dong-khoa/quan-ly" element={<HoatDongKhoaQuanLy />} />
+      <Route path="hoat-dong-khoa/duyet" element={<HoatDongKhoaDuyet />} />
+      <Route path="hoat-dong-khoa/xac-nhan" element={<HoatDongKhoaXacNhan />} />
+      <Route path="yeu-cau-chi-doan" element={<YeuCauChiDoan />} />
+      <Route path="gui-yeu-cau" element={<GuiYeuCau />} />
+      <Route path="bao-cao" element={<BaoCao />} />
+    </Route>
   </Route>
 );
 
